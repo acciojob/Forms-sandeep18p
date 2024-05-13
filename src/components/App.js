686966f76ef1f2d2e9671592aafa58aa2995330d
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-14 00:29:18 +0530

    two

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 
-export default function App(){
-  return (<Router>
-    <div>
+function App() {
+  return (
+    <Router> 
+      <div>
         <nav>
-            <Link to="/form" id="form-link">   Section 1</Link>
-            <Link to="/form-ref" id="form-ref-link">   Section 2</Link>
-            <Link to="/form-state" id="form-state-link">   Section 3</Link>
+          <ul>
+            <li>
+               <Link to="/form" id="form-link">
+                Section 1
+              </Link>
+            </li>
+            <li>
+              <Link to="/form-ref" id="form-ref-link">
+                Section 2
+              </Link>
+            </li>
+            <li>
+              <Link to="/form-state" id="form-state-link">
+                Section 3
+              </Link>
+            </li>
+          </ul>
         </nav>
-    </div>
-    <Routes>
-          <Route path="/form" element={<Section1/>} />
-          <Route path="/form-ref" element={<Section2/>} />
-          <Route path="/form-state" element={<Section3/>} />
+
+        <Routes>
+          <Route path="/form" element={<Section1 />} />
+          <Route path="/form-ref" element={<Section2 />} />
+          <Route path="/form-state" element={<Section3 />} />
         </Routes>
-  </Router>);
+      </div>
+    </Router>
+  );
 }
+
+export default App;
